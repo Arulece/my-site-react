91828598fa750b6b30dc09548cf689f3d08b0746
Arul kumar <arulk@adobe.com>
2026-02-03 19:42:54 +0530

    Add copy-webpack-plugin to copy assets to dist folder for production

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
@@ -62,6 +63,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
       filename: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public', 'assets'),
+          to: 'assets',
+        },
+      ],
     }),
   ],
 };
